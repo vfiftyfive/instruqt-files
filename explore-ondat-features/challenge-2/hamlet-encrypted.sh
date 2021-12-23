5a2eca773b73cb430b8deb48bcac812540199f0a
vfiftyfive <vfiftyfive@gmail.com>
2021-12-23 10:28:08 +0000

    amend sts deploy scripts

--- explore-ondat-features/challenge-2/hamlet-encrypted.sh
+++ explore-ondat-features/challenge-2/hamlet-encrypted.sh
@@ -2,4 +2,8 @@
 
 storageos cordon ondat-server
 storageos cordon ondat-worker2
+sleep 5
 kubectl apply -f ~/ondat/hamlet-encrypted.yaml
+sleep 8
+storageos uncordon ondat-server
+storageos uncordon ondat-worker2
--- explore-ondat-features/challenge-2/hamlet-unencrypted.sh
+++ explore-ondat-features/challenge-2/hamlet-unencrypted.sh
@@ -2,4 +2,9 @@
 
 storageos cordon ondat-server
 storageos cordon ondat-worker1
+sleep 5
 kubectl apply -f ~/ondat/hamlet-unencrypted.yaml
+sleep 8
+kubectl label pvc $(kubectl get pvc -o name | grep "\-encrypted") "storageos.com/replicas=0"
+storageos uncordon ondat-server
+storageos uncordon ondat-worker1
