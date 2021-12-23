7ad459b1246ea64762128489e61a0365fde1e5aa
vfiftyfive <vfiftyfive@gmail.com>
2021-12-23 14:57:02 +0000

    change sts scripts

--- explore-ondat-features/challenge-2/hamlet-encrypted.sh
+++ explore-ondat-features/challenge-2/hamlet-encrypted.sh
@@ -4,6 +4,6 @@ kubectl label node ondat-server "storageos.com/computeonly=true" >/dev/null 2>&1
 kubectl label node ondat-worker2 "storageos.com/computeonly=true" >/dev/null 2>&1 
 sleep 5
 kubectl apply -f ~/ondat/hamlet-encrypted.yaml
-sleep 8
+sleep 10
 kubectl label node ondat-server "storageos.com/computeonly"- >/dev/null 2>&1
 kubectl label node ondat-worker2 "storageos.com/computeonl"- >/dev/null 2>&1 
--- explore-ondat-features/challenge-2/hamlet-unencrypted.sh
+++ explore-ondat-features/challenge-2/hamlet-unencrypted.sh
@@ -4,7 +4,7 @@ kubectl label node ondat-server "storageos.com/computeonly=true" >/dev/null 2>&1
 kubectl label node ondat-worker1 "storageos.com/computeonly=true" >/dev/null 2>&1 
 sleep 5
 kubectl apply -f ~/ondat/hamlet-unencrypted.yaml
-sleep 8
 kubectl label "$(kubectl get pvc -o name | grep "\-unencrypted")" "storageos.com/replicas=0"
+sleep 10
 kubectl label node ondat-server "storageos.com/computeonly"- >/dev/null 2>&1
 kubectl label node ondat-worker1 "storageos.com/computeonl"- >/dev/null 2>&1 
