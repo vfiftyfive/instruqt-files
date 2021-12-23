aa36f6433f4469395af9f214438dd1262a3fb948
vfiftyfive <vfiftyfive@gmail.com>
2021-12-23 15:09:49 +0000

    fix typos

--- explore-ondat-features/challenge-2/hamlet-encrypted.sh
+++ explore-ondat-features/challenge-2/hamlet-encrypted.sh
@@ -6,4 +6,4 @@ sleep 5
 kubectl apply -f ~/ondat/hamlet-encrypted.yaml
 sleep 10
 kubectl label node ondat-server "storageos.com/computeonly"- >/dev/null 2>&1
-kubectl label node ondat-worker2 "storageos.com/computeonl"- >/dev/null 2>&1 
+kubectl label node ondat-worker2 "storageos.com/computeonly"- >/dev/null 2>&1 
--- explore-ondat-features/challenge-2/hamlet-unencrypted.sh
+++ explore-ondat-features/challenge-2/hamlet-unencrypted.sh
@@ -7,4 +7,4 @@ kubectl apply -f ~/ondat/hamlet-unencrypted.yaml
 kubectl label "$(kubectl get pvc -o name | grep "\-unencrypted")" "storageos.com/replicas=0"
 sleep 10
 kubectl label node ondat-server "storageos.com/computeonly"- >/dev/null 2>&1
-kubectl label node ondat-worker1 "storageos.com/computeonl"- >/dev/null 2>&1 
+kubectl label node ondat-worker1 "storageos.com/computeonly"- >/dev/null 2>&1 
