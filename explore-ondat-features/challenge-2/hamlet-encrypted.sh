7a62235c17212f69f32d4e215e7a72f5623133ca
vfiftyfive <vfiftyfive@gmail.com>
2021-12-23 12:22:18 +0000

    amend sts deploy scripts

--- explore-ondat-features/challenge-2/hamlet-encrypted.sh
+++ explore-ondat-features/challenge-2/hamlet-encrypted.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-storageos cordon ondat-server &> /dev/null
-storageos cordon ondat-worker2 &> /dev/null
+storageos cordon ondat-server >/dev/null 2>&1 
+storageos cordon ondat-worker2 >/dev/null 2>&1
 sleep 5
 kubectl apply -f ~/ondat/hamlet-encrypted.yaml
 sleep 8
-storageos uncordon ondat-server &> /dev/null
-storageos uncordon ondat-worker2 &> /dev/null
+storageos uncordon ondat-server >/dev/null 2>&1
+storageos uncordon ondat-worker2 >/dev/null 2>&1
--- explore-ondat-features/challenge-2/hamlet-unencrypted.sh
+++ explore-ondat-features/challenge-2/hamlet-unencrypted.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-storageos cordon ondat-server &> /dev/null
-storageos cordon ondat-worker1 &> /dev/null
+storageos cordon ondat-server >/dev/null 2>&1
+storageos cordon ondat-worker1 >/dev/null 2>&1
 sleep 5
 kubectl apply -f ~/ondat/hamlet-unencrypted.yaml
 sleep 8
 kubectl label "$(kubectl get pvc -o name | grep "\-unencrypted")" "storageos.com/replicas=0"
-storageos uncordon ondat-server &> /dev/null
-storageos uncordon ondat-worker1 &> /dev/null
+storageos uncordon ondat-server >/dev/null 2>&1
+storageos uncordon ondat-worker1 >/dev/null 2>&1
