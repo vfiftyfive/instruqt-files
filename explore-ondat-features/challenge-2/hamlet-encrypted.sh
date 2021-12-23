e049440d7cda90ee1adf3866df130a5b345a8831
vfiftyfive <vfiftyfive@gmail.com>
2021-12-23 14:54:05 +0000

    change sts scripts

--- explore-ondat-features/challenge-2/hamlet-encrypted.sh
+++ explore-ondat-features/challenge-2/hamlet-encrypted.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-storageos cordon ondat-server >/dev/null 2>&1 
-storageos cordon ondat-worker2 >/dev/null 2>&1
+kubectl label node ondat-server "storageos.com/computeonly=true" >/dev/null 2>&1 
+kubectl label node ondat-worker2 "storageos.com/computeonly=true" >/dev/null 2>&1 
 sleep 5
 kubectl apply -f ~/ondat/hamlet-encrypted.yaml
 sleep 8
-storageos uncordon ondat-server >/dev/null 2>&1
-storageos uncordon ondat-worker2 >/dev/null 2>&1
+kubectl label node ondat-server "storageos.com/computeonly"- >/dev/null 2>&1
+kubectl label node ondat-worker2 "storageos.com/computeonl"- >/dev/null 2>&1 
--- explore-ondat-features/challenge-2/hamlet-unencrypted.sh
+++ explore-ondat-features/challenge-2/hamlet-unencrypted.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-storageos cordon ondat-server >/dev/null 2>&1
-storageos cordon ondat-worker1 >/dev/null 2>&1
+kubectl label node ondat-server "storageos.com/computeonly=true" >/dev/null 2>&1 
+kubectl label node ondat-worker1 "storageos.com/computeonly=true" >/dev/null 2>&1 
 sleep 5
 kubectl apply -f ~/ondat/hamlet-unencrypted.yaml
 sleep 8
 kubectl label "$(kubectl get pvc -o name | grep "\-unencrypted")" "storageos.com/replicas=0"
-storageos uncordon ondat-server >/dev/null 2>&1
-storageos uncordon ondat-worker1 >/dev/null 2>&1
+kubectl label node ondat-server "storageos.com/computeonly"- >/dev/null 2>&1
+kubectl label node ondat-worker1 "storageos.com/computeonl"- >/dev/null 2>&1 
